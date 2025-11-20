105c6f3e6e7724ec14322b0d731d7caed1f16375
Fares Mahmoud <faressafi203@gmail.com>
2025-11-20 12:54:49 +0200

    fix: images remote patterns warning and middleware warning fix

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const sessionCookie = request.cookies.get("better-auth.session_token");
 
   if (!sessionCookie && request.nextUrl.pathname.startsWith("/admin")) {
